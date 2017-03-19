ee8d2f6d3b165d8c12649eec8c76428a162b759a
Jeffrey Chen <jeffreychen41@gmail.com>
2017-03-18 22:06:07 -0700

    Created an auto refresh for new tweets for each user

--- application.js
+++ application.js
@@ -2,71 +2,44 @@
 
 $(document).ready(function(){
 
-
+// display all users
 var getTweets = (function() {
   var users = streams.users;
   for (var i in users) {
 
       $('#content').append('<div id=each><il class= "user ' +[i]+'"> @' +
-      [i] +":<a href='#' class=expand> more tweets</a><ul class=messages>"+
-       groupTweets(i) + "</ul></div></div>");
-
+      [i] +':<a href="#" class=expand> Timeline</a>'+
+      '<ul class=messages><ul class= "messages'+i+'"></ul></ul></il></div>');
 
   }
 })
 
-function groupTweets(i) {
-//console.log(streams.users[i]);
-var array = [];
-  streams.users[i].forEach(function(tweet) {
-
-  array.push(tweet.message + '<ul class=time><i>Created: ' + tweet.created_at + '</i></ul>');
-  })
-  //console.log(array);
-  return array.join('<hr>');
-}
-
 
 getTweets();
+//file:///Users/JeffsMac/hrsf75-twittler/index.html
+// run the generator
 
 
-$('.expand').on('click',function(event) {
-  event.stopPropagation();
-    event.preventDefault();
-  $(this).closest('.user').find('.messages').slideToggle();
-  })
+// show timeline of each user
+// includes Auto add of tweets for each
 
-$('.user').on('click', function() {
-  console.log('1');
-})
+  setInterval(function Update(){
 
-/*
-users.forEach(function(tweet) {
-  $('#content').append('<div id=each><div class= '+tweet.user+'> @' +
-  tweet.user +':</div><div class=messages>'+
-   tweet.message+ '</div><ul class=time><i>Created: '+tweet.created_at+'</i></ul></div>');
-})
-*/
-/*
-var allUserTweets = function(){
-var users = streams.users;
-for (var name in users) {
-    name = '.'+ name;
-    name = name.toString();
-    $(name).on('click', function() {
-      return renderTweets(name);
-    });
-}
-}
-
-var renderTweets = function(tweetArray) {
-  console.log(tweetArray);
-  streams.user[tweetArray].forEach(function(tweets) {
-    $('#content').prepend('<div class=messages>' + tweets.messages+'</div>');
-  })
-}
-//allUserTweets();
-//var autoUpdate =
-*/
+  var users = streams.users;
+    for (var i in users) {
+      var $body = $('.messages'+i);
+      $body.html('');
+          streams.users.shawndrost.forEach(function(index){
+            //console.log(index);
+          var $tweet= $('<div></div>');
+          $tweet.text(index.message +' Created At:'+index.created_at);
+          $tweet.appendTo($body);
+        })
+      }
+    },10000)
 
 });
+
+
+
+*/
